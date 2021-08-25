4b6f7ae6bab0929aea792aacc14c63823a6eb478
Eric Huang <eric.haoran.huang@gmail.com>
2021-08-25 14:16:31 -0400

    Got the preliminary stuff going

--- src/goose-counter/main.py
+++ src/goose-counter/main.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import (
+    Flask, request, Response
+)
+import http.client
+import sys
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/goose", methods = ["GET", "POST"])
+def goose():
+    if request.method == "POST":
+        conn = http.client.HTTPConnection("nickname-generator", 8081)
+        conn.request("GET", "/")
+        res = conn.getresponse()
+
+        if res.status == 200:
+            # add in some good old headers
+            response = Response(res.read())
+            response.headers["Access-Control-Allow-Origin"] = "*"
+            response.headers["Content-Type"] = "application/json"
+            response.status = 200
+            return response
+
+        return "internal server error", 500
+    elif request.method == "GET":
+        return "hello there"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
--- src/nickname-generator/main.rkt
+++ src/nickname-generator/main.rkt
@@ -2,12 +2,32 @@
 (require web-server/servlet
          web-server/servlet-env)
 
+(define names 
+    (list
+        'fred
+        'goose
+        'yoda
+        'groot
+        'water
+        'violin
+        'ball
+        'grass
+        'siesta
+    ))
+
+; returns a random name using names above
+(define (get-random-name)
+    (list-ref names (random (length names))))
+
+(define (get-name-json-bytes)
+    (string->bytes/utf-8 (format "{ \"name\": ~s }" (symbol->string (get-random-name)))))
+
 (define (get-response)
     (response
-        301 #"OK"
+        200 #"OK"
         (current-seconds) TEXT/HTML-MIME-TYPE
         empty
-        (λ (op) (write-bytes #"<html><body>Hello, World!</body></html>" op))))
+        (λ (op) (write-bytes (get-name-json-bytes) op))))
 
 (define (start req)
     (get-response))
