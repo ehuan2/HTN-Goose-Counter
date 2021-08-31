1b832b29c342e8e7e63e601c19c7eda34e1a9774
Eric Huang <eric.haoran.huang@gmail.com>
2021-08-31 16:44:52 -0400

    set up the api from the python side

---
+++ src/goose-counter/redis_client.py
@@ -0,0 +1,19 @@
+# this module handles all the redis stuff
+import redis
+import util
+
+# when we save the stuff to redis, 
+def saveGoose(goose):
+    r = redis.Redis(host="redis", port=6379, db=0)
+    r.rpush("geese", goose)
+    r.close()
+
+def getGeese():
+    r = redis.Redis(host="redis", port=6379, db=0)
+    geese = []
+    for bytes in r.lrange("geese", 0, -1):
+        # unmarshal geese to remarshal with the counter
+        geese.append(util.unmarshalGoose(bytes.decode()))
+    counter = r.llen("geese")
+    r.close()
+    return util.marshalGeeseAndCounter(geese, counter) 
---
+++ src/goose-counter/util.py
@@ -0,0 +1,33 @@
+# this module here will define some utility functions for http requests and json marshaling
+from flask import Response
+import json
+
+# given the gooseJson, it'll add necessary headers for the response
+def getResponse(goose):
+    response = Response(goose)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Content-Type"] = "application/json"
+    response.status = 200
+    return response
+
+# marshals the nickname and the location into json
+def marshalGoose(nickname, coordinates):
+    return json.dumps({
+        "nickname": nickname,
+        "coordinates": {
+            "x": coordinates[0],
+            "y": coordinates[1]
+        }
+    })
+
+def unmarshalGoose(goose):
+    return json.loads(goose)
+
+def unmarshalNickname(nickname):
+    return json.loads(nickname)
+
+def marshalGeeseAndCounter(geese, counter):
+    return json.dumps({
+        "geese": geese,
+        "counter": counter
+    })
